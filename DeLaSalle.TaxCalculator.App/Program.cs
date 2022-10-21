bec70912d889cb9cdff0ca89d4975e78551e6873
arturoacevedo <arturoacevedo@hotmail.com>
2022-10-20 19:00:07 -0500

    changes 20102022

--- DeLaSalle.TaxCalculator.App/Program.cs
+++ DeLaSalle.TaxCalculator.App/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using DeLaSalle.TaxCalculator.App.Taxes;
+using DeLaSalle.TaxCalculator.Core.Entities;
+using static System.Double;
+
+Console.WriteLine("Please type your salary: ");
+#pragma warning disable CA1806
+TryParse(Console.ReadLine(), out var amount);
+#pragma warning restore CA1806
+
+var salary = new Salary{ Amount = amount};
+var isrCalculatorService = new IsrCalculatorService();
+var isr = isrCalculatorService.GetIsr(salary);
+
+Console.WriteLine($" ISR : {isr} ");
